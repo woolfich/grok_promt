67252cfeafe77c314d688a961b1bf50a5eca3ca9
woolfich <peresver@gmail.com>
2025-10-04 15:03:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/grok_promt/', // Укажи имя твоей репы
+  base: './', // ✅ Относительный путь — работает всегда на GitHub Pages
 });
